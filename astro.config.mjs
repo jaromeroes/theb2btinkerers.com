8c250c181d930bbf4553523512a622d4b463f299
jaromeroes <jaromero.es@gmail.com>
2026-03-01 18:12:22 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://theb2btinkerers.netlify.app',
+  site: 'https://theb2btinkerers.com',
   integrations: [sitemap()],
 });
